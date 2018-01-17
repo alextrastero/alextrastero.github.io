e3e7e0571f630de233c69b68613a332b381ff63e
Alexander Odell <alextrastero@gmail.com>
2018-01-17 23:00:19 +0100

    Extract js

---
+++ main.js
@@ -0,0 +1,21 @@
+(() => {
+  var style = `
+    font-family: 'Georgia';
+    font-weight: 600;
+    font-size: 1.5em;
+    background: -webkit-linear-gradient(33deg, #283c86, #45a247);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: #283c86;
+  `;
+
+  const log = (text) => console.log(`%c ${text[0]} `, style);
+
+  log`Alexander Garth Odell`
+  log`Frontend Developer based in Berlin`
+  log`Current: babbel`
+  log`Previous: wimdu | gameduell | betvictor`
+  log`+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+`
+  log`github.com/alextrastero`
+  log`linkedin.com/in/alexandergarthodell`
+})()
